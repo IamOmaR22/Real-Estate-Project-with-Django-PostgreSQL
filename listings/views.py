180f8056c9de5f33ba2fada69bdd562120bbbef3
Md. Omar Faruk <iamomar022@gmail.com>
2020-02-07 00:56:07 +0600

    Initial Commit

---
+++ listings/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'listings/listings.html')
+
+
+def listing(request):
+    return render(request, 'listings/listing.html')
+
+
+def search(request):
+    return render(request, 'listings/search.html')
