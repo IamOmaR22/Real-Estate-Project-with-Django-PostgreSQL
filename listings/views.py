3cb048dcb2abb9f81ae4ef41546e16c77c188fdd
Md. Omar Faruk <iamomar022@gmail.com>
2020-02-12 23:28:28 +0600

    Search complete for all categories

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,6 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator  ## For
 from .choices import price_choices, bedroom_choices, state_choices   ## no need listings, because already imported Listing
 
 
-# Create your views here.
 
 def index(request):
 
@@ -38,10 +37,49 @@ def listing(request, listing_id):
 
 def search(request):
 
+    queryset_list = Listing.objects.order_by('-list_date')  ## Get all of the listings
+
+    #### Keywords (Search/Filtering by using keyword of search box) ####
+    if 'keywords' in request.GET:   ## Check keyword exist
+        keywords = request.GET['keywords']  ## Get the actual form value
+
+        if keywords: ## To make sure it's not an empty string
+            ##--## Keywords for descriptions(description__icontains, it will take the whole paragraph) ##--##
+            queryset_list = queryset_list.filter(description__icontains=keywords) ##icontains, it takes whole paragraph
+
+
+    #### City (Search/Filtering by using City of search box) ####
+    if 'city' in request.GET:   ## Check city exist
+        city = request.GET['city']
+        if city:   ## iexact is case insensitive and exact is case sensitive
+            queryset_list = queryset_list.filter(city__iexact=city) ##iexact, it takes only the exact name of city
+
+    #### State ####
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    #### Bedrooms ####
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms) ## Suppose, 4 bedrooms(Upto). lte means less then or equalto. if greater then it won't show
+
+    #### Price ####
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+### keywords, city, state, bedrooms, price all they are the name( name = keywords etc ) in select tags in html
+
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET     ### Preserving form input
     }
 
     return render(request, 'listings/search.html', context)
