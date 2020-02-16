40911a87756dcef1fa0ef5c20fcaef33d1033288
Md. Omar Faruk <iamomar022@gmail.com>
2020-02-17 00:21:31 +0600

    Message Alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages ## For message
+
 
 # Create your views here.
 
 def register(request):
 
     if request.method == 'POST':  ### Form Submission (<form action="{% url 'register' %}" method="POST">)
-        # Register User
-        return
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
--- realstate/static/js/main.js
+++ realstate/static/js/main.js
@@ -1,2 +1,12 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+
+// Message Alert Disappear Start
+setTimeout(function () {
+
+    $('#message').fadeOut('slow');
+    
+}, 3000);   // after 3 seconds it will disappear
+// Message Alert Disappear End
+// After this need to run python manage.py collectstatic
