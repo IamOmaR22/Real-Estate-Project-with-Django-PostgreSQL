a7f026a85aa87763da6993defc18bbfc74df8209
Md. Omar Faruk <iamomar022@gmail.com>
2020-03-28 22:35:35 +0600

    Contact app register and admin customization

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+admin.site.register(Contact)
