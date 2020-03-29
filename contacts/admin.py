45c0561fe6d9b3038c3154a13b5114336c490310
Md. Omar Faruk <iamomar022@gmail.com>
2020-03-29 10:30:50 +0600

    Top bar done

--- contacts/admin.py
+++ contacts/admin.py
@@ -10,4 +10,4 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'listing')
     list_per_page = 25
 
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
